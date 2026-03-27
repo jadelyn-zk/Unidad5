c66196cf8dccfc529f75c5e52a9545300d37b8d5
Jade <jade.belloso@iesnervion.es>
2026-03-27 21:38:04 +0100

    hecho todo

--- EjerciciosHerencia/src/ejercicio3/Perecedero.java
+++ EjerciciosHerencia/src/ejercicio3/Perecedero.java
@@ -1,5 +1,39 @@
 package ejercicio3;
 
-public class Perecedero {
+public class Perecedero extends Producto {
+    private int diasACaducar;
 
+    public Perecedero(String nombre, double precio, int diasACaducar) {
+        super(nombre, precio);
+        this.diasACaducar = diasACaducar;
+    }
+
+    public int getDiasACaducar()            { 
+    	return diasACaducar; 
+    	}
+    
+    public void setDiasACaducar(int dias)   {
+    	this.diasACaducar = dias; 
+    }
+    
+    @Override
+    public double calcular(int cantidad) {
+        double total = super.calcular(cantidad);
+        if (diasACaducar == 1) {
+        	total= total / 4;
+        }
+        else if (diasACaducar == 2) {
+        	total= total / 3;
+        }
+        else if (diasACaducar == 3) {
+        	total= total / 2;
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto perecedero: " + getNombre() + ", Precio: " + getPrecio()
+             + ", Días para que caduque: " + diasACaducar;
+    }
 }
